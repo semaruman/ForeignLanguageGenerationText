950d489684f55002ecabf2f98d21ac0048b86217
Semen <semen.ruman@gmail.com>
2026-02-22 15:54:49 +0300

    Добавил авторизацию пользователя

--- ForeignLanguageGenerationText/Areas/User/Controllers/HomeController.cs
+++ ForeignLanguageGenerationText/Areas/User/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using ForeignLanguageGenerationText.Areas.User.Models;
+﻿using CodeCareer.Areas.User.Models;
+using ForeignLanguageGenerationText.Areas.User.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ForeignLanguageGenerationText.Areas.User.Controllers
@@ -17,5 +18,58 @@ namespace ForeignLanguageGenerationText.Areas.User.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Authorization()
+        {
+            return View(currentUser);
+        }
+
+        [HttpPost]
+        public IActionResult Authorization(UserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                UserModelDb db = new UserModelDb();
+
+                var dbUser = db.GetUserModels().Where(u => u.UserName == user.UserName).FirstOrDefault();
+
+
+
+                // Если пользователь не найден в БД
+                if (dbUser == null)
+                {
+                    currentUser = new UserModel
+                    {
+                        UserName = user.UserName,
+                        Password = user.Password,
+                        LearnedWords = new List<string>()
+                    };
+                    db.AddUserModel(currentUser);
+                    return RedirectToAction("SuccessAuthorization");
+                }
+                else
+                {
+                    if (dbUser.Password == user.Password)
+                    {
+                        currentUser = dbUser;
+                        return RedirectToAction("SuccessAuthorization");
+                    }
+                    else
+                    {
+                        ViewBag.WrongPassword = "Неверный пароль!";
+                        return View(user);
+                    }
+                }
+            }
+            else
+            {
+                return View(user);
+            }
+        }
+
+        public IActionResult SuccessAuthorization()
+        {
+            return View();
+        }
     }
 }
--- ForeignLanguageGenerationText/Areas/User/Models/UserModel.cs
+++ ForeignLanguageGenerationText/Areas/User/Models/UserModel.cs
@@ -1,10 +1,21 @@
 ﻿
 
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
 namespace ForeignLanguageGenerationText.Areas.User.Models
 {
     public class UserModel
     {
+        [Required(ErrorMessage = "Введите имя пользователя")]
+        [MinLength(2, ErrorMessage = "Имя пользователя должно быть не менее 2-х символов")]
+        [MaxLength(30, ErrorMessage = "Имя пользователя должно быть не более 30 символов")]
         public string UserName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Введите пароль")]
+        public string Password { get; set; } = string.Empty;
+
+        [BindNever]
         public List<string> LearnedWords { get; set; } = new List<string>();
     }
 }
