da901ae997157b1f71d1fe9a87d8f82e6e25e8a4
Semen <semen.ruman@gmail.com>
2026-02-23 13:09:18 +0300

    Настроил маршрутизацию

--- ForeignLanguageGenerationText/Program.cs
+++ ForeignLanguageGenerationText/Program.cs
@@ -3,9 +3,13 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+app.MapGet("/", () => Results.Redirect("/User/Index"));
+
+// Маршрут для других областей
 app.MapControllerRoute(
     name: "areas",
     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-    );
+);
+
 
 app.Run();
